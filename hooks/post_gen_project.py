b46a42b1ebf7541046c17c94b0164f9d7f312f43
Maxime Bouroumeau-Fuseau <maxime.bouroumeau@gmail.com>
2025-10-16 12:46:34 +0200

    updates for production docker

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,11 +3,8 @@ import random
 
 def generate_secret_key():
     secret_key = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
-    with open(".env", "r") as f:
-        content = f.read()
     with open(".env", "w") as f:
-        content = content.replace("FLASK_SECRET_KEY=changeme", f"FLASK_SECRET_KEY={secret_key}")
-        f.write(content)
+        f.write(f"FLASK_SECRET_KEY={secret_key}")
 
 
 if __name__ == "__main__":
